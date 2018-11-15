660cb1f4b64d64003a17af6ed5091ede5b8762cd
Paulo andrade <source.compu@gmail.com>
2018-11-15 10:19:04 -0600

    Agregamos detalles a las vistas

--- Autotransportes ABC/Controllers/HomeController.cs
+++ Autotransportes ABC/Controllers/HomeController.cs
@@ -12,33 +12,5 @@ namespace Autotransportes_ABC.Controllers
         {
             return View();
         }
-
-        public ActionResult AddAuto()
-        {
-            ViewBag.Message = "Agregamos un dato a la base de datos";
-
-            return View();
-        }
-
-        public ActionResult AddSolicitud()
-        {
-            ViewBag.Message = "Agregamos una solicitud a la base de datos";
-
-            return View();
-        }
-
-        public ActionResult Autos()
-        {
-            ViewBag.Message = "Your auto page.";
-
-            return View();
-        }
-
-        public ActionResult Solicitudes()
-        {
-            ViewBag.Message = "Your solicitud page.";
-
-            return View();
-        }
     }
 }
